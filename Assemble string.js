1f6f65718cdb8fcb167ad23fd5d0d345943f89d4
Haarith <banisabur@gmail.com>
2023-04-11 09:05:09 +0700

    Assemble string.js

---
+++ Assemble string.js
@@ -0,0 +1,47 @@
+// In this task, you need to restore a string from a list of its copies.
+
+// You will receive an array of strings. All of them are supposed to be the same as the original but, unfortunately, they were corrupted which means some of the characters were replaced with asterisks ("*").
+
+// You have to restore the original string based on non-corrupted information you have. If in some cases it is not possible to determine what the original character was, use "#" character as a special marker for that.
+
+// If the array is empty, then return an empty string.
+
+// Examples:
+// input = [
+//   "a*cde",
+//   "*bcde",
+//   "abc*e"
+// ]
+// result = "abcde"
+
+
+// input = [
+//   "a*c**",
+//   "**cd*",
+//   "a*cd*"
+// ]
+// result = "a#cd#"
+
+function assembleString(arr) {
+    if (arr.length === 0) {
+      return "";
+    }
+    const n = arr[0].length;
+    let result = "";
+    for (let i = 0; i < n; i++) {
+      const charSet = new Set();
+      for (const s of arr) {
+        if (s[i] !== "*") {
+          charSet.add(s[i]);
+        }
+      }
+      if (charSet.size === 1) {
+        result += charSet.values().next().value;
+      } else if (charSet.size === 0) {
+        result += "#";
+      } else {
+        result += "#";
+      }
+    }
+    return result;
+  }
