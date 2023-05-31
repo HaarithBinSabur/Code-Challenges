2704cf70a8767a7ca977002648c26168d3f91b86
Haarith <banisabur@gmail.com>
2023-05-31 09:07:24 +0700

    Training JS #6

---
+++ Training JS #6.js
@@ -0,0 +1,10 @@
+// Getting Started:
+// You have chosen to translate this kata. For your convenience we have provided the existing test cases used for the language that you have already completed, as well as all of the other related fields.
+
+// View the help tab for more information on how kata translations work.
+
+
+function trueOrFalse(val){
+    if (val) return "true";
+    else     return "false";
+  }
