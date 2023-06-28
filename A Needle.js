ca6809ae5a40b86038fe049a6c6d6eff9508aa8b
Haarith <banisabur@gmail.com>
2023-06-28 17:05:59 +0700

    A Needle in a Haystack

---
+++ A Needle.js
@@ -0,0 +1,20 @@
+// Can you find the needle in the haystack?
+
+// Write a function findNeedle() that takes an array full of junk but containing one "needle"
+
+// After your function finds the needle it should return a message (as a string) that says:
+
+// "found the needle at position " plus the index it found the needle, so:
+
+// Example(Input --> Output)
+
+// ["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"] --> "found the needle at position 5" 
+
+function findNeedle(haystack) {
+    var index = haystack.findIndex(function(element) {
+      return element === "needle";
+    })
+    var place = "found the needle at position " + index;
+    return place;
+  }
+  
