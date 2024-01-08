e26a0ba29d6b7d72dcc78ed266e29a2d9e69423e
Haarith <banisabur@gmail.com>
2024-01-08 10:25:56 +0700

    Shifty Clousures

---
+++ Shifty Clousures.js
@@ -0,0 +1,26 @@
+// Functional closures can get overly attached. Set them straight!
+
+// Why doesn't greetAbe() actually greet Abe?
+
+//===
+
+/* This is a debugg problem. that task is simple but maybe in the words placed up above it is made diffiucult.
+I myself did not understand what this problem actually ment. 
+Because the problem is not wrong it just wants you to get the results of greetAbe(). So how do you solve it?
+Erase Everything thats there and replace it with somthing that will console log both Ben and Abe.
+Theres many ways to approch this actually ^_^*/ 
+
+
+
+// let name = 'Abe'
+
+// const greetAbe = () => 'Hello, ' + name + '!'
+
+// name = 'Ben'
+
+// const greetBen = () => 'Hello, ' + name + '!'
+
+
+
+const greetAbe = () => 'Hello, Abe!';
+const greetBen = () => 'Hello, Ben!';
